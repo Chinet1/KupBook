78ca7d297adce15cfd645162f813d5231e91301d
Mateusz Zbylut <contact@mateuszzbylut.com>
2019-05-06 16:21:10 +0200

    Navbar

--- index.php
+++ index.php
@@ -10,6 +10,29 @@
     <link rel="stylesheet" href="asset/css/style.css">
 </head>
 <body>
-
+<nav class="navbar navbar-expand-lg navbar-dark bg-36">
+    <a class="navbar-brand" href="#">
+        <img src="img/logo.png" alt="Logo">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Strona Główna <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Zaloguj się</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Zarejestruj się</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Przeglądaj zasoby</a>
+            </li>
+        </ul>
+    </div>
+</nav>
 </body>
 </html>
