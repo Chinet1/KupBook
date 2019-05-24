c8ca198fac2fa6922d774aad06839a52f861f591
Mateusz Zbylut <contact@mateuszzbylut.com>
2019-05-24 15:51:59 +0200

    Login with activated account

--- login.php
+++ login.php
@@ -15,20 +15,31 @@
         if (empty($_POST['login']) || empty($_POST['password'])) {
             echo '<script>alert("Uzupełnij oba pola formularza logowania!")</script>';
         } else {
-            if ($query = $con->prepare('SELECT id, password, role FROM `Users` WHERE email = ?')) {
+            if ($query = $con->prepare('SELECT id, password, role, activation_code FROM `Users` WHERE email = ?')) {
                 $query->bind_param('s', $_POST['login']);
                 $query->execute();
                 $query->store_result();
 
                 if ($query->num_rows > 0) {
-                    $query->bind_result($id, $password, $role);
+                    $query->bind_result($id, $password, $role, $activation_code);
                     $query->fetch();
                     if (password_verify($_POST['password'], $password)) {
-                        session_regenerate_id();
-                        $_SESSION['loggedin'] = TRUE;
-                        $_SESSION['name'] = $_POST['login'];
-                        $_SESSION['id'] = $id;
-                        header('Location: index.php');
+                        $query = $con->prepare('SELECT name, last_name FROM `Clients` WHERE ID = ?');
+                        $query->bind_param('i', $id);
+                        $query->execute();
+                        $query->store_result();
+                        $query->bind_result($name, $last_name);
+                        $query->fetch();
+                        if ($activation_code == 'activated') {
+                            session_regenerate_id();
+                            $_SESSION['loggedin'] = TRUE;
+                            $_SESSION['email'] = $_POST['login'];
+                            $_SESSION['name'] = $name . ' ' . $last_name;
+                            $_SESSION['id'] = $id;
+                            header('Location: index.php');
+                        } else {
+                            echo '<script>alert("Konto nie zostało jeszcze aktywowane!")</script>';
+                        }
                     } else {
                         echo '<script>alert("Nieprawodiłowe hasło!")</script>';
                     }
