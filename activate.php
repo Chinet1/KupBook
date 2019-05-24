8fda4c98a972fdeb39f6ef626c41adbf41bf5484
Mateusz Zbylut <contact@mateuszzbylut.com>
2019-05-24 15:50:31 +0200

    Activate account script

---
+++ activate.php
@@ -0,0 +1,27 @@
+<?php
+include 'model.php';
+session_start();
+
+if ($_SESSION['loggedin']) {
+    header('Location: index.php');
+}
+
+if (isset($_GET['email'], $_GET['code'])) {
+    if ($stmt = $con->prepare('SELECT * FROM `Users` WHERE email = ? AND activation_code = ?')) {
+        $stmt->bind_param('ss', $_GET['email'], $_GET['code']);
+        $stmt->execute();
+        $stmt->store_result();
+        if ($stmt->num_rows > 0) {
+            if ($stmt = $con->prepare('UPDATE `Users` SET activation_code = ? WHERE email = ? AND activation_code = ?')) {
+                $newcode = 'activated';
+                $stmt->bind_param('sss', $newcode, $_GET['email'], $_GET['code']);
+                $stmt->execute();
+                echo '<script>alert("Konto aktywowane propawnie, możesz się zalogować!")</script>';
+                header('Location: login.php');
+            }
+        } else {
+            echo '<script>alert("Błąd, konto aktywowane lub nie istnieje!")</script>';
+            header('Location: index.php');
+        }
+    }
+}
