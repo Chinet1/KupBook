0686c88431f7f3d70e14d64bdef7a5d1ece300b8
Mateusz Zbylut <contact@mateuszzbylut.com>
2019-05-31 19:45:41 +0200

    Added fontawesome dependency

--- partials/header.php
+++ partials/header.php
@@ -9,3 +9,4 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
     <link rel="stylesheet" href="asset/css/bootstrap.min.css">
     <link rel="stylesheet" href="asset/css/style.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
